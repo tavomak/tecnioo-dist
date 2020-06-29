f27a82f49b3e3edbc865b7a306b0c63aae76f3ec
Gustavo Rivero OQO-lap <gustavo.rivero@oqo.cl>
2020-06-29 01:30:41 -0400

    UX Rutas/Calendario

--- assets/js/routes-dist.js
+++ assets/js/routes-dist.js
@@ -1,21 +1,79 @@
 $(function () {
 
-    $('#btnDatepicker').on('click', function(){
+    $('#btnDatepicker').on('click', function () {
         $('#datepickerRoutes').toggleClass('d-none');
-    } )
-    $( "#datepickerRoutes" ).datepicker();
-
-    $('.routeCard').on('click', function(e) {
-        e.preventDefault();
-        $('.card-day-3 .badge-las-condes').toggleClass('bg-info');
-        if ($(".card-day-3 .btn").length ) {
-            $('.cardBtnAsignacion').toggleClass('d-none')
-        }else {
-            $('.card-day-3').append(`
-                <button class="btn btn-outline-primary btn-sm mt-2 cardBtnAsignacion"> Asignar </button>
-            `)
-        }
     })
+    $("#datepickerRoutes").datepicker();
+
+    const TIME = new Date(),
+    TODAY = TIME.getDay();
+
+    $(`.weekDay-${TODAY}`).css({
+        'color': 'white',
+        'background' : '#007bff'
+    });
+
+    $(`.weekDay-${TODAY} .nav-item`).addClass('text-white');
+    $(`.weekDay-${TODAY} #activeDay`).removeClass('d-none');
+
+    $('.routeCard').on('click', function (e) {
+        e.preventDefault(); //Prevenimos el comportamiento por defecto del evento
+        $('html, body').animate({
+            scrollTop: 0
+        }, 100);
+        $('.routeCardGroup').fadeOut();
+
+        //Si la tarjeta se activa se agrega un sombreado
+        if ($(this).parents('.routeCardGroup').is('.shadow')) {
+            $('.routeCardGroup').removeClass('shadow');
+            $('#pills-home').find('.wrap').fadeIn()
+            $('.routeCardGroup').fadeIn();
+
+        } else {
+            $('.routeCardGroup').removeClass('shadow');
+            $(this).parents('.routeCardGroup').fadeIn();
+            $(this).parents('.routeCardGroup').addClass('shadow');
+
+            //Deselecciona todos los badges activos
+            $('.badge--container .badge').removeClass('bg-info');
+            //Oculta los botones activos
+            $('.card-day').find('.cardBtnAsignacion').addClass('d-none')
+
+            let rutas = $(this).data('rutas');
+            //Si la maquina tiene rutas asignadas
+            if (rutas.length) {
+                $('#pills-home').find('.wrap').fadeOut()
+
+                $.each(rutas, function (k, v) {
+                    $('#pills-home').find('.ruta' + v).fadeIn()
+                })
+            } else {
+                $('#pills-home').find('.wrap').fadeOut()
+
+                setTimeout(function () {
+                    $('.wrap-active').fadeIn()
+                }, 0);
+            }
+        }
+
+        //Seleccina y resalta las comunas
+        let comuna = $(this).data('comuna');
+        $(`[data-comuna=${comuna}]`).toggleClass('bg-info');
+        $('.wrap').removeClass('wrap-active');
+        $(`[data-comuna=${comuna}]`).parents('.wrap').addClass('wrap-active');
+
+        //muestra u oculta el boton
+        $(`[data-comuna=${comuna}]`).parents('.card-day').find('.cardBtnAsignacion').toggleClass('d-none')
+    })
+
+    $('.routeCardGroup select').on('change', function () {
+        let value = this.value;
+        if (value != "Seleccionar Ruta") {
+            $(this).parents('.routeCardGroup').find('.btn').removeClass('d-none')
+        } else {
+            $(this).parents('.routeCardGroup').find('.btn').addClass('d-none')
+        }
+    });
 
 
 }); 
